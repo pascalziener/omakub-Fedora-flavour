da8c6622002cf352a1cec04c57fcd328af1f576d
Pascal Ziener <ziener.dev@gmail.com>
2024-06-21 21:45:23 +0200

    Migrated app-alacritty.sh to Fedora

--- install/app-alacritty.sh
+++ install/app-alacritty.sh
@@ -1,4 +1,4 @@
-sudo apt install -y alacritty
+sudo dnf install -y alacritty
 mkdir -p ~/.config/alacritty
 cp ~/.local/share/omakub/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
 cp ~/.local/share/omakub/configs/alacritty/theme.toml ~/.config/alacritty/theme.toml
