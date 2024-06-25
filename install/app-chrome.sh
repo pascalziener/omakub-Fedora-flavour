e588e69f973a7ce3fd29b0296db28e0b15ec3082
Pascal Ziener <ziener.dev@gmail.com>
2024-06-25 22:44:05 +0200

    Migrated app-chrome.sh to Fedora using the Fedora's third-party repositories

--- install/app-chrome.sh
+++ install/app-chrome.sh
@@ -1,6 +1,5 @@
-cd /tmp
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install -y ./google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
+sudo dnf install -y fedora-workstation-repositories
+sudo dnf install -y google-chrome-stable
+
 xdg-settings set default-web-browser google-chrome.desktop
-cd -
+
